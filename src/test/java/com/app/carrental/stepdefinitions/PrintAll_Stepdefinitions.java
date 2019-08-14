a521f0a7c0d4ba981bed6b9df1e41e62236a58b6
kiraz26 <halilkirazz@gmail.com>
2019-08-13 22:31:41 -0400

    first commit

---
+++ src/test/java/com/app/carrental/stepdefinitions/PrintAll_Stepdefinitions.java
@@ -0,0 +1,44 @@
+package com.app.carrental.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PrintAll_Stepdefinitions {
+	@Given("User get request from url")
+	public void user_get_request_from_url() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("Content Type and Accept type is JSON")
+	public void content_Type_and_Accept_type_is_JSON() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("User can see all blue Teslas")
+	public void user_can_see_all_blue_Teslas() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Status code is {int}")
+	public void status_code_is(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("User can see notes about blue Teslas")
+	public void user_can_see_notes_about_blue_Teslas() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("JSON Response data should match the actual data")
+	public void json_Response_data_should_match_the_actual_data() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+}
---
+++ src/test/resources/com/app/features/PrintAll.feature
@@ -0,0 +1,10 @@
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  Scenario: Title of your scenario
+    Given User get request from url
+    And Content Type and Accept type is JSON
+    When User can see all blue Teslas
+    And Status code is 200
+    Then User can see notes about blue Teslas
+    Then JSON Response data should match the actual data
